1fe8f9618fa64380c73df7d3b76e0da017e6d9f7
席恒子 <785160953@qq.com>
2018-01-15 15:15:04 +0800

    增加：列属性

--- src/main/java/xin/xihc/jba/annotation/Column.java
+++ src/main/java/xin/xihc/jba/annotation/Column.java
@@ -28,14 +28,60 @@ public @interface Column {
 		NONE, AUTO, GUID/* 小写 */, GUID_UP/* 大写 */, SEQ;
 	}
 
+	/**
+	 * 列名
+	 * 
+	 * @return
+	 */
 	public String value();
 
+	/**
+	 * 默认值
+	 * 
+	 * @return
+	 */
+	public String defaultValue() default "";
+
+	/**
+	 * 是否允许为空
+	 * 
+	 * @return
+	 */
+	public boolean notNull() default false;
+
+	/**
+	 * 是否是主键
+	 * 
+	 * @return
+	 */
 	public boolean primary() default false;
 
+	/**
+	 * 主键生成策略
+	 * 
+	 * @return
+	 */
 	public PrimaryPolicy policy() default PrimaryPolicy.AUTO;
 
+	/**
+	 * 是否唯一
+	 * 
+	 * @return
+	 */
 	public boolean unique() default false;
 
-	public int length();
+	/**
+	 * 长度限制-1代表不限制
+	 * 
+	 * @return
+	 */
+	public int length() default -1;
+
+	/**
+	 * 备注
+	 * 
+	 * @return
+	 */
+	public String remark() default "";
 
 }
