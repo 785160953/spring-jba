c583e6e9f012b9f7ecf80759a0049d754134c676
Leo.Xi <785160953@qq.com>
2019-10-24 13:24:33 +0800

    修正：多数据源情况下异常的问题

--- src/main/java/xin/xihc/jba/core/utils/BasicPageSqlBuilder.java
+++ src/main/java/xin/xihc/jba/core/utils/BasicPageSqlBuilder.java
@@ -28,33 +28,12 @@ public class BasicPageSqlBuilder {
      * @date 2019/9/27
      * @since 0.0.1
      */
-    protected String build(final String sql, PageInfo pageInfo, String databaseName) {
+    public String build(final String sql, PageInfo pageInfo, String databaseName) {
         if ("MySQL".equals(databaseName)) {
             // 计算起始索引
             // 使用limit 0, 10分页 -- 索引从0开始
             return sql + " LIMIT " + pageInfo.getStart() + "," + pageInfo.getPageSize();
         }
-        return toBuild(sql, pageInfo, databaseName);
-    }
-
-    /**
-     * 抽象方法，可自定义基础分页
-     *
-     * @param sql          原始查询sql语句
-     * @param pageInfo     分页信息
-     * @param databaseName 数据库产品名
-     *                     "Apache Derby",
-     *                     "DB2",
-     *                     "MySQL",
-     *                     "Microsoft SQL Server",
-     *                     "Oracle",
-     *                     "PostgreSQL"
-     * @return 分页sql
-     * @author Leo.Xi
-     * @date 2019/9/27
-     * @since 0.0.1
-     */
-    public String toBuild(final String sql, PageInfo pageInfo, String databaseName) {
         return sql;
     }
 
